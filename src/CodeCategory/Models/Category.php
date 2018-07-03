14ff2792699324ed9d98d5b8ea2d4ee081570e43
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-03 13:49:10 -0300

    Testando relacionamentos

--- src/CodeCategory/Models/Category.php
+++ src/CodeCategory/Models/Category.php
@@ -17,4 +17,14 @@ class Category extends Model
         'name', 'active', 'parent_id'
     ];
 
+    public function parent()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
 }
