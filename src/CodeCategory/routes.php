738f73509ef26c4494ffc41a9a8ab141bd5de5c1
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-13 11:41:51 -0300

    Criando testes com usuario autenticado

--- src/CodeCategory/routes.php
+++ src/CodeCategory/routes.php
@@ -3,6 +3,7 @@
 Route::name('admin.')
         ->prefix('admin/')
         ->middleware('web')
+        ->middleware('auth')
         ->namespace('CodePress\CodeCategory\Controllers')
         ->group(function () {
             Route::resources([
